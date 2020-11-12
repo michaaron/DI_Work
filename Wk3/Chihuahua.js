2f401841276cbb8993d42565a57f5c1f5fe26e5a
michaaron <mstettin1@gmail.com>
2020-11-12 13:41:11 +0200

    Xp

---
+++ Wk3/Chihuahua.js
@@ -0,0 +1,15 @@
+let newDog = "Chihuahua";
+console.log(newDog.length)
+
+// let l = newDog.length;
+// console.log(l)
+//another way to show length
+
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+if (newDog == "Chihuahua") {
+    console.log("I love Chihuahua, it's my favorite dog breed")
+} else {
+    console.log("I don't care, I prefer cats")
+}
---
+++ Wk3/simple.js
@@ -0,0 +1,10 @@
+let x = 20 
+let y = 13
+
+if (x > y) {
+    console.log("x is the biggest number")
+} else if (x == y) {
+    console.log("these are the same!")
+} else if (x < y) {
+    console.log("y is actually the biggest number")
+}
